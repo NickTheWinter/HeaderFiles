742922cb50e7a72069a04062d96fd62dcf22c4ff
Зименковни <@101-12>
2022-09-29 12:33:36 +0300

    add length of segment calc

--- HeaderFiles/Header.h
+++ HeaderFiles/Header.h
@@ -1,10 +1,13 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #define FIRST  "first "
 #define SECOND "second"
-#define _CRT_SECURE_NO_WARNINGS
+
 float answer;
+float SegmentLength(float x1, float x2, float y1, float y2);
 float Plus(float, float);
 float Minus(float, float);
 float Devide(float, float);
@@ -16,11 +19,11 @@ void Print(float sum)
 float Multiply(float a, float b)
 {
 	return a * b;
-}/*
+}
 char* IntToString(int n) 
 {
 	char str[6];
 	return sprintf(str, "%d", n);
-}*/
+}
 
 
--- HeaderFiles/Source.c
+++ HeaderFiles/Source.c
@@ -1,7 +1,7 @@
 #include "Header.h"
 #define A 10.0
 #define B 5.0
-
+#define AMP(C) C * C
 int main()
 {
 	system("chcp 1251>nul");
@@ -18,16 +18,27 @@ int main()
 	char* third = FIRST SECOND;
 	printf("\nќтвет: %s\n", third);
 
-	printf("¬ведите число, которое необходимо преобразовать в строку\n");
+	/*printf("¬ведите число, которое необходимо преобразовать в строку\n");
 	int number = 0;
-	scanf_s("%d", &number);/*
-	printf("\nЁто число будет: %s",IntToString(number));*/
+	scanf_s("%d", &number);
+	char* tmp = IntToString(number);
+	for (size_t i = 0; i < 5; i++)
+	{
+		printf("\nЁто число будет: %s", tmp[i]);
+	}
 
 	printf("\n¬ведите строку, которую необходимо преобразовать в число\n");
 	char* str = "";
 	scanf_s("%s", &str);
-	printf("%d", str);
+	printf("%d", str);*/
+	float x1 = 0, x2 = 1, y1 = 0, y2 = 1;
+	printf("ƒлина отрезка по координатам {%.2f;%.2f},{%.2f;%.2f} = %.2f\n", x1, y1, x2, y2, SegmentLength(x1, x2, y1, y2));
+
 	system("pause");
+}
+float SegmentLength(float x1, float x2, float y1, float y2)
+{
+	return sqrt(AMP(x2 - x1) + AMP(y2 - y1));
 }
 float Plus(float a, float b)
 {
